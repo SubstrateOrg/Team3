ea3c08dde84aac825f1c0343b4f7a6687eef0617
romakingwolf <romakingwolf@126.com>
2019-09-22 23:43:01 +0800

    Fix overflow problem in creating a kitty, and add breed a new kitty function.

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -24,14 +24,46 @@ decl_module! {
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
 			let count = Self::kitties_count();
-			if count == u32::max_value() {
+			/*if count == u32::max_value() {
 				return Err("Kitties count overflow");
-			}
+			}*/
+			// check whether count + 1 overflow
+			let updated_count = count.checked_add(1).ok_or("Kitties count overflow")?;
+
 			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
 			let dna = payload.using_encoded(blake2_128);
 			let kitty = Kitty(dna);
 			Kitties::insert(count, kitty);
-			KittiesCount::put(count + 1);
+			//KittiesCount::put(count + 1);
+			KittiesCount::put(updated_count);
+		}
+
+		/// breed a new child kitty from two existed kitties
+		pub fn breed(origin, index1: u32, index2: u32) {
+			let sender = ensure_signed(origin)?;
+
+			if index1 == index2 || index1 == u32::max_value() || index2 == u32::max_value() {
+				return Err("Invalid parent kitties ID");
+			}
+			let kitty1 = Self::kitty(index1);
+			let kitty2 = Self::kitty(index2);
+
+			let count = Self::kitties_count();
+			let updated_count = count.checked_add(1).ok_or("Kitties count overflow")?;
+
+			//calc child kitty dna
+			let kitty1_dna = kitty1.0;
+			let kitty2_dna = kitty2.0;
+			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+			let dna = payload.using_encoded(blake2_128);
+			let mut child_dna = [0u8; 16];
+			for i in 0..kitty1_dna.len() {
+				child_dna[i] = (!dna[i] & kitty1_dna[i]) | (dna[i] & kitty2_dna[i]);
+			}
+
+			let child_kitty = Kitty(child_dna);
+			Kitties::insert(count, child_kitty);
+			KittiesCount::put(updated_count);
 		}
 	}
 }
