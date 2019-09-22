dfd08ce3838f31e2b2fa4cc2ba2ce398086c52b1
493123121@qq.com <493123121@qq.com>
2019-09-22 15:06:14 +0800

    繁殖kitty

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -33,5 +33,36 @@ decl_module! {
 			Kitties::insert(count, kitty);
 			KittiesCount::put(count + 1);
 		}
+
+		pub fn create_child(origin, kitty1_id: [u8; 16], kitty2_id: [u8; 16]) {
+		    let sender = ensure_signed(origin)?;
+
+		    ensure!(Kitties::exists(kitty1_id), "kitty 1 不存在");
+		    ensure!(Kitties::exists(kitty2_id), "kitty 2 不存在");
+
+            let count = Self::kitties_count();
+
+            ensure!(u32::max_value() <= count, "不能创建 kitty，数量超限")
+
+		    let kitty_1 = Self::kitty(kitty_id_1);
+            let kitty_2 = Self::kitty(kitty_id_2);
+
+            let mut final_dna = kitty_1;
+
+            let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+            let dna = payload.using_encoded(blake2_128);
+
+            for (i, (dna_2_element, r)) in kitty_2.as_ref().iter().zip(dna.as_ref().iter()).enumerate() {
+                if r % 2 == 0 {
+                    final_dna.as_mut()[i] = *dna_2_element;
+                }
+            }
+
+            let kitty = Kitty(final_dna);
+            Kitties::insert(count, kitty);
+			KittiesCount::put(count + 1);
+
+			Ok(())
+		}
 	}
 }
