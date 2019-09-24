59c2c87ff101a30d583d4d31c33bb9fdf2074cbf
ximc <ximinchao@163.com>
2019-09-24 17:15:08 +0800

    implement new dna

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -5,6 +5,7 @@ use runtime_io::blake2_128;
 use system::ensure_signed;
 use rstd::result;
 
+
 pub trait Trait: system::Trait {
 	type KittyIndex: Parameter + SimpleArithmetic + Bounded + Default + Copy;
 }
@@ -29,47 +30,49 @@ decl_storage! {
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		/// Create a new kitty
-		pub fn create(origin) {
+		pub fn create(origin) -> Result {
 			let sender = ensure_signed(origin)?;
 
-			// 作业：重构create方法，避免重复代码
+			let new_kitty_id = Self::next_kitty_id()?;
+			let new_dna = Self::random_value(&sender);
 
-			let kitty_id = Self::kitties_count();
-			if kitty_id == T::KittyIndex::max_value() {
-				return Err("Kitties count overflow");
-			}
+			Self::insert_kitty(sender, new_kitty_id, Kitty(new_dna));
 
-			// Generate a random 128bit value
-			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
-			let dna = payload.using_encoded(blake2_128);
-
-			// Create and store kitty
-			let kitty = Kitty(dna);
-			<Kitties<T>>::insert(kitty_id, kitty);
-			<KittiesCount<T>>::put(kitty_id + 1.into());
-
-			// Store the ownership information
-			let user_kitties_id = Self::owned_kitties_count(&sender);
-			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
-			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+			Ok(())
 		}
 
 		/// Breed kitties
-		pub fn breed(origin, kitty_id_1: T::KittyIndex, kitty_id_2: T::KittyIndex) {
+		pub fn breed(origin, kitty_id_1: T::KittyIndex, kitty_id_2: T::KittyIndex) -> Result {
 			let sender = ensure_signed(origin)?;
 
 			Self::do_breed(sender, kitty_id_1, kitty_id_2)?;
+
+			Ok(())
 		}
 	}
 }
 
 fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
-	// 作业：实现combine_dna
-	// 伪代码：
-	// selector.map_bits(|bit, index| if (bit == 1) { dna1 & (1 << index) } else { dna2 & (1 << index) })
-	// 注意 map_bits这个方法不存在。只要能达到同样效果，不局限算法
-	// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
-	return dna1;
+	let mut new_dna = 0;
+	let mut bit_mask: u8 = 0x01;
+
+	runtime_io::print(dna1);
+	runtime_io::print(dna2);
+	runtime_io::print(selector);
+
+	for i in 0..8 {
+		if (bit_mask & selector) != 0 {
+			new_dna |= dna1 & bit_mask;
+		} else {
+			new_dna |= dna2 & bit_mask;
+		}
+
+		bit_mask <<= 1;
+	}
+
+	runtime_io::print(new_dna);
+
+	return new_dna;
 }
 
 impl<T: Trait> Module<T> {
@@ -92,7 +95,7 @@ impl<T: Trait> Module<T> {
 		<KittiesCount<T>>::put(kitty_id + 1.into());
 
 		// Store the ownership information
-		let user_kitties_id = Self::owned_kitties_count(owner.clone());
+		let user_kitties_id = Self::owned_kitties_count(&owner);
 		<OwnedKitties<T>>::insert((owner.clone(), user_kitties_id), kitty_id);
 		<OwnedKittiesCount<T>>::insert(owner, user_kitties_id + 1.into());
 	}
